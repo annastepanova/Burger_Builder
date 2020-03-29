a88ec9d30c8e2a4164d79b02ef661ef9815ec89a
annastepanova <annastepanova1811@gmail.com>
2020-03-29 16:31:56 -0400

    added props validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './BurgerIngredient.module.css'
+import PropTypes from 'prop-types'
 
-const BurgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+
+  render() {
+
   let ingredient = null;
 
   switch (props.type) {
-    case('bread-bottom'):
+    case ('bread-bottom'):
       ingredient = <div className={styles.breadBottom}></div>
       break;
-    case('bread-top'): 
-    ingredient = (
-    <div className={styles.breadTop}>
-      <div className={styles.seeds1}></div>
-      <div className={styles.seeds1}></div>
-    </div>
-    )
-    break;
-    case('meat'): 
+    case ('bread-top'):
+      ingredient = (
+        <div className={styles.breadTop}>
+          <div className={styles.seeds1}></div>
+          <div className={styles.seeds1}></div>
+        </div>
+      )
+      break;
+    case ('meat'):
       ingredient = <div className={styles.meat}></div>
       break;
     case ('cheese'):
@@ -28,12 +32,13 @@ const BurgerIngredient = (props) => {
     case ('bacon'):
       ingredient = <div className={styles.bacon}></div>
       break;
-      default: 
+    default:
       ingredient = null;
   }
 
   return ingredient
 
+  }
 }
-
+  
 export default BurgerIngredient

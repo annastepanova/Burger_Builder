0b9845c57ae9531a5e0b0e7a03044f9d45cfa934
annastepanova <annastepanova1811@gmail.com>
2020-03-30 15:57:03 -0400

    render burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
 
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />
+      })
+    })
+
   return (
 
     <div className={styles.burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
 
--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,20 @@ import Burger from '../../Burger/Burger'
 
 class BurgerBuilder extends Component {
 
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 1,
+      meat: 1
+    }
+  }
+
   render() {
 
     return (
       <>
-      <Burger/>
+      <Burger ingredients={this.state.ingredients}/>
       <div>Build Controls</div>
       </>
     )

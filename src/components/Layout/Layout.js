18b26e62190e125ace62adb75b283367e70e6322
annastepanova <annastepanova1811@gmail.com>
2020-04-03 18:59:40 -0400

    added Backdrop to SideDrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navbar from '../Navigation/Navbar/Navbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import styles from './Layout.module.css'
 
-const Layout = (props) => (
-  <>
-  <Navbar/>
-  <SideDrawer/>
-  <main className={styles.content}>{props.children}</main>
-  </>
-)
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+
+  }
+
+  render () {
+    return (
+      <>
+        <Navbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <main className={styles.content}>{this.props.children}</main>
+      </>
+    )
+  }
+}
+
 
 export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 import styles from './SideDrawer.module.css'
 
 const SideDrawer = (props) => {
+let attachedStyles = [styles.sideDrawer, styles.close]
 
+if (props.open) {
+  attachedStyles = [styles.sideDrawer, styles.open]
+}
   return (
-
-    <div className={styles.sideDrawer}>
-      <div className={styles.logo}><Logo /></div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    <>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedStyles.join(' ')}>
+        <div className={styles.logo}><Logo /></div>
+        <nav>
+          <NavigationItems/>
+        </nav>
+      </div>
+    </>
 
   )
 }

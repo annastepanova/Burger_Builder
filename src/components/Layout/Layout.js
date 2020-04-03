d740e5938d488bc85c519f958be33e80c66d3259
annastepanova <annastepanova1811@gmail.com>
2020-04-03 15:56:46 -0400

    created SideDrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navigation/Navbar/Navbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import styles from './Layout.module.css'
 
 const Layout = (props) => (
   <>
   <Navbar/>
+  <SideDrawer/>
   <main className={styles.content}>{props.children}</main>
   </>
 )

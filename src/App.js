e0e599961eaf1b892895cd5d9ee24802dcb2599f
annastepanova <annastepanova1811@gmail.com>
2020-04-06 15:39:01 -0400

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './components/containers/Checkout/Checkout'
 
 function App() {
   return (
-    <div>
-      <Layout>
-        <BurgerBuilder/>
-        <Checkout/>
-      </Layout>
-    </div>
+    <BrowserRouter>
+      <div>
+          <Layout>
+            <Route exact path="/" component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout} />
+          </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 

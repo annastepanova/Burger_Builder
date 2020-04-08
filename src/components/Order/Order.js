f2b751be360af2af684bab9bf1243064c9fd1ece
annastepanova <annastepanova1811@gmail.com>
2020-04-07 21:23:48 -0400

    outputting orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import styles from './Order.module.css'
 
-const Order = (props) => (
+const Order = (props) => {
 
-  <div className={styles.order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 10.00</strong></p>
+  const ingredients = []
+  for (let ingredient in props.ingredients) {
+    ingredients.push({name: ingredient, amount: props.ingredients[ingredient]})
+  }
 
-  </div>
-)
+  const ingredientOutput = ingredients.map(ing => {
+    return (
+      <span className={styles.span} key={ing.name}>{ing.name} ({ing.amount})</span>
+    )
+  })
+
+  return (
+    <div className={styles.order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>USD {Number(props.price).toFixed(2)}</strong></p>
+    </div>
+  )
+}
 
 export default Order

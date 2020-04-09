5318f9d82dd94836227a066b1ac53d403d723638
annastepanova <annastepanova1811@gmail.com>
2020-04-09 18:13:05 -0400

    added form validation

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Your name'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             street: {
               elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Street'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             zipCode: {
               elementType: 'input',
@@ -31,7 +39,13 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'ZipCode'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true,
+                minLength: 5,
+                maxLength: 5
+              },
+              valid: false
             },
             country: {
               elementType: 'input',
@@ -39,7 +53,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Country'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             email: {
               elementType: 'input',
@@ -47,7 +65,11 @@ class ContactData extends Component {
                 type: 'email',
                 placeholder: 'Your e-mail'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             deliveryMethod: {
               elementType: 'select',
@@ -62,6 +84,8 @@ class ContactData extends Component {
             },
             loading: false
   }
+
+
 
   orderHandler = (event) => {
     event.preventDefault()
@@ -86,13 +110,30 @@ class ContactData extends Component {
 
   }
 
+  checkValidation = (value, rules) => {
+    let isValid = true
+    
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+    return isValid
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
     }
     const formElement = {...updatedOrderForm[inputIdentifier]}
     formElement.value = event.target.value
+    formElement.valid = this.checkValidation(formElement.value, formElement.validation)
     updatedOrderForm[inputIdentifier] = formElement
+    console.log(formElement)
     this.setState({orderForm: updatedOrderForm})
 
   }
